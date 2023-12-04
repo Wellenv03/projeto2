df0addc69ba547f91fda601f78c9294daec587cf
Wellenv03 <149198211+Wellenv03@users.noreply.github.com>
2023-12-04 15:28:33 -0300

    Projeto 2
    Desafio DIO

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log ("jodador numero 1")
+
+let.nikiname ("pastel")
+
+const mensagem = "jogador numero 1"+ let.nikiname
+
+let.saldoDeVitorias
+
+//declarando operadores
+let.numero = "0, 10"
+console.log (saldoDeVitorias + numero)
+console.log (ferro)
+
+ calcular_ranked (vitorias, derrotas)
+    saldo_vitorias = vitorias - derrotas
+
+    ifsaldoDeVitorias < 10 (nivel) + "Ferro"
+
+    return saldo_vitorias, nivel
+
+
+vitorias = int(input("7: "))
+derrotas = int(input("3: "))
+
+saldo, nivel = calcular_ranked(vitorias, derrotas)
+
+print.log("O Herói tem um saldo de {7 pontos} e está no nível de {ferro}")
